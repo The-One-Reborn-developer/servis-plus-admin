a97b5c8079d0927bcfac4ffa44482947d53834f6
The One Reborn <mortonmarrion@gmail.com>
2024-12-30 16:54:57 +0300

    feat: setup demo environment

---
+++ launchers/demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+# Bring down the existing demo_admin environment
+sudo docker-compose -p demo_admin -f dockerfiles/docker-compose-demo.yml down -v
+
+# Build the demo_admin images
+sudo docker-compose -p demo_admin -f dockerfiles/docker-compose-demo.yml build --no-cache
+
+# Bring up the demo_admin environment
+sudo docker-compose -p demo_admin -f dockerfiles/docker-compose-demo.yml up
