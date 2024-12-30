cf92ce5ab99b7a2449ae228377dc69f5748461dc
The One Reborn <mortonmarrion@gmail.com>
2024-12-30 12:01:47 +0300

    feat: setup dev environment

---
+++ launchers/dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+# Bring down the existing dev_admin environment
+sudo docker-compose -p dev_admin -f dockerfiles/docker-compose-dev.yml down -v
+
+# Build the dev_admin images
+sudo docker-compose -p dev_admin -f dockerfiles/docker-compose-dev.yml build --no-cache
+
+# Bring up the dev_admin environment
+sudo docker-compose -p dev_admin -f dockerfiles/docker-compose-dev.yml up
