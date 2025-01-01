1e280d1fea69392b9da26cb8c069228a52928a00
The One Reborn <mortonmarrion@gmail.com>
2025-01-01 13:14:26 +0300

    debug: console log session adding result

--- app/web/static/scripts/dashboard.js
+++ app/web/static/scripts/dashboard.js
@@ -316,13 +316,13 @@ function addNewRowForm(container, service) {
             });
 
             const result = await response.json();
-
+            console.log(result)
             if (result.success) {
                 alert('Игровая сессия успешно добавлена!');
                 await fetchTableData(service, 'Игровые сессии'); // Refresh the table
             } else {
-                alert(`Ошибка: ${result}`);
-            }
+                alert(`Ошибка: ${result.message}`);
+            };
         } catch (error) {
             console.error('Error adding new row:', error);
             alert('Произошла ошибка при добавлении новой строки. Пожалуйста, попробуйте еще раз.');
