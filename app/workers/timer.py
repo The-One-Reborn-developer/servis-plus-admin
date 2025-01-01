2cd4d37bfe1ee539e3e6142183a8f8eea38b66cc
The One Reborn <mortonmarrion@gmail.com>
2025-01-01 21:09:15 +0300

    fix: add seconds margin

--- app/workers/timer.py
+++ app/workers/timer.py
@@ -3,8 +3,7 @@ import logging
 
 from datetime import (
     datetime,
-    timedelta,
-    timezone
+    timedelta
 )
 
 from zoneinfo import ZoneInfo
@@ -14,6 +13,7 @@ from app.database.queries.update_game_session import update_game_session
 
 
 MOSCOW_TIMEZONE = ZoneInfo('Europe/Moscow')
+MARGIN_SECONDS = 1
 
 
 def timer_worker():
@@ -22,26 +22,24 @@ def timer_worker():
             now = datetime.now(MOSCOW_TIMEZONE)
             sessions = get_pending_game_sessions()
             next_event_time = None
-
+            logging.info(f"Checking session {session['id']}: Now: {now}, Start: {session_date}, End: {end_time}")
             if not sessions:
                 logging.info("No game sessions to check")
                 time.sleep(60)
                 continue
 
             for session in sessions:
-                session_date = datetime.fromisoformat(session['session_date']).replace(tzinfo=MOSCOW_TIMEZONE)
+                session_date = datetime.fromisoformat(session['session_date']).astimezone(MOSCOW_TIMEZONE)
                 end_time = session_date + timedelta(minutes=session['countdown_timer'])
-                logging.info(f"Checking game session {session['id']} with date: {session['session_date']}. now: {now}. end_time: {end_time}")
+                
                 # If session hasn't started and it's time to start
                 if not session['started'] and session_date <= now <= end_time:
                     update_game_session(session['id'], 'started')
                     logging.info(f"Game session {session['id']} has started")
-
                 # If session has started but not finished and timer has run out
-                elif session['started'] and end_time <= now:
+                elif session['started'] and (end_time - timedelta(seconds=MARGIN_SECONDS)) <= now:
                     update_game_session(session['id'], 'finished')
                     logging.info(f"Game session {session['id']} has finished")
-
                 # Determine the nearest next event time
                 if not session['started']:
                     next_event_time = min(next_event_time or session_date, session_date)
