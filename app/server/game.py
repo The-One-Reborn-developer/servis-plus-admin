36024361acf0dc3bee69e7ac75c45a139a33c3af
The One Reborn <mortonmarrion@gmail.com>
2025-01-01 13:17:18 +0300

    debug: try current app logger instead of print

--- app/server/game.py
+++ app/server/game.py
@@ -37,7 +37,7 @@ def add_session():
         return redirect(url_for('admin.login'))
 
     session_date = request.form.get('session-date')
-    print(f'session_date: {session_date}')
+    current_app.logger.info(f'session_date: {session_date}')
     return jsonify({
         'session_date': session_date,
         'type': str(type(session_date))
