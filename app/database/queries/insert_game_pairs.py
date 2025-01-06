f0dbe04fd6cbb928753a209a89ad59ac3b966045
The One Reborn <mortonmarrion@gmail.com>
2025-01-06 22:12:27 +0300

    fix: change unpacking logic

--- app/database/queries/insert_game_pairs.py
+++ app/database/queries/insert_game_pairs.py
@@ -12,14 +12,13 @@ def insert_game_pairs(pairs, session_id):
     with sync_session() as session:
         with session.begin():
             logging.info(f'Inserting {pairs} game pairs for session {session_id}')
-            session.execute(
-                insert(GamePair),
-                    [
-                        {
-                            'session_id': session_id,
-                            'round': 1,
-                            'player1_telegram_id': player1,
-                            'player2_telegram_id': player2
-                        } for (player1, player2) in pairs
-                    ]
+            game_pair_objects = [
+                GamePair(
+                    session_id=session_id,
+                    round=1,
+                    player1_telegram_id=player1_telegram_id,
+                    player2_telegram_id=player2_telegram_id
                 )
+                for (player1_telegram_id, player2_telegram_id) in pairs
+            ]
+            session.add_all(game_pair_objects)
