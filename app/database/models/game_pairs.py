6f3440d2ec72d7fea005d60a1abda9081aba3b51
The One Reborn <mortonmarrion@gmail.com>
2025-01-06 22:17:49 +0300

    fix: modify GamePair object for new fields

--- app/database/models/game_pairs.py
+++ app/database/models/game_pairs.py
@@ -7,9 +7,12 @@ from app.database.models.base import Base
 class GamePair(Base):
     __tablename__ = 'game_pairs'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    game_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    session_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    round: Mapped[str] = mapped_column(Integer, nullable=False)
     player1_telegram_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     player2_telegram_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    player1_choice: Mapped[str] = mapped_column(Integer, nullable=True)
+    player2_choice: Mapped[str] = mapped_column(Integer, nullable=True)
     winner_telegram_id: Mapped[int] = mapped_column(BigInteger, nullable=True)
 
     def to_dict(self):
--- app/database/queries/insert_game_pairs.py
+++ app/database/queries/insert_game_pairs.py
@@ -11,14 +11,15 @@ def insert_game_pairs(pairs, session_id):
         raise ValueError("session_id is required")
     with sync_session() as session:
         with session.begin():
-            logging.info(f'Inserting {pairs} game pairs for session {session_id}')
-            game_pair_objects = [
-                GamePair(
-                    session_id=session_id,
-                    round=1,
-                    player1_telegram_id=player1_telegram_id,
-                    player2_telegram_id=player2_telegram_id
+            logging.info(f'Inserting {len(pairs)} game pairs for session {session_id}')
+            session.execute(
+                insert(GamePair),
+                    [
+                        {
+                            'session_id': session_id,
+                            'round': '1',
+                            'player1_telegram_id': player1_telegram_id,
+                            'player2_telegram_id': player2_telegram_id
+                        } for player1_telegram_id, player2_telegram_id in pairs
+                    ]
                 )
-                for (player1_telegram_id, player2_telegram_id) in pairs
-            ]
-            session.add_all(game_pair_objects)
